636f6a40e560d3d495b0e06fb644d0e8a4056c1c
miguelgallo <miguelgallo@users.noreply.github.com>
2019-10-26 01:36:46 -0300

    Update sub_dy_resample.sh

--- CMSSW_10_6_0/src/workspace/Dilepton/condor/sub_dy_resample.sh
+++ CMSSW_10_6_0/src/workspace/Dilepton/condor/sub_dy_resample.sh
@@ -10,21 +10,3 @@ run(true, 100)
 EOS
 mv output_cut.root histos_dy_resample/$file
 rm input.root
-
-cp /eos/user/m/malvesga/miniaod/GGToMuMu_Pt-50_Elastic_13TeV-lpair/output_elastic_xangle130.root input.root
-root -b -l <<EOS
-.L ntp1-miniaod_MC.C
-run()
-.q
-EOS
-mv output_cut.root histos_MC/output_elastic_xangle130_cut.root
-rm input.root
-
-cp /eos/user/m/malvesga/miniaod/GGToMuMu_Pt-50_Inel-El_13TeV-lpair/output_inel-el_xangle130.root input.root
-root -b -l <<EOS
-.L ntp1-miniaod_MC.C
-run()
-.q
-EOS
-mv output_cut.root histos_MC/output_inel-el_xangle130_cut.root
-rm input.root
